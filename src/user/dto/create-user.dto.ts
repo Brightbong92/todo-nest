f132d58024e9b93e92caba5799d9dc6776390511
brightbong92 <wkdgusqhd080@naver.com>
2023-05-02 20:24:51 +0900

    feat swagger add

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,30 @@
 import { IsString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDTO extends PartialType(User) {
+  @ApiProperty({
+    example: 'test@test.com',
+    description: '유저 이메일',
+    required: true,
+  })
   @IsString()
   readonly email: string;
+
+  @ApiProperty({
+    example: '홍길동',
+    description: '유저 이름',
+    required: true,
+  })
   @IsString()
   readonly name: string;
+
+  @ApiProperty({
+    example: 'password',
+    description: '유저 비밀번호',
+    required: true,
+  })
   @IsString()
   readonly password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
+@ApiTags('유저 API')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({ summary: '유저 생성 API', description: '유저를 생성한다.' })
+  @ApiCreatedResponse({
+    description: '유저를 생성한다.',
+    schema: {
+      example: { success: true },
+    },
+  })
   createUser(@Body() userData: CreateUserDTO) {
     return this.userService.createUser(userData);
   }
