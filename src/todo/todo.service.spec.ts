f89e57c235200c0e531dda18c10338c9645c9d7e
brightbong92 <wkdgusqhd080@naver.com>
2023-05-03 21:28:01 +0900

    Test todoService jest unit test

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -16,4 +16,28 @@ describe('TodoService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAll', () => {
+    it('it should be a return Array', async () => {
+      const result = await service.findAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('findOne', () => {
+    it('it should be return a todo', async () => {
+      const id = await service.create({
+        title: 'New Todo',
+        description: 'New Description',
+        createdAt: new Date(),
+      });
+
+      const result = await service.findOne(id);
+      expect(result).toBeDefined();
+      expect(result.id).toEqual(id);
+      expect(result.title).toEqual('New Todo');
+      expect(result.description).toEqual('New Description');
+      await service.deleteOne(id);
+    });
+  });
 });
